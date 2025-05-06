4b28612152fcd935a97d14b225c8cfaa8d0bcc7d
Goku <.@cicinnatus.edu.do>
2025-05-06 16:23:42 -0400

    aaaaA

--- pruebas javscript/4- strings.js
+++ pruebas javscript/4- strings.js
@@ -8,13 +8,26 @@ console.log(color);
 // EJEMPLO 2
 let fruta = 'manzana';
 
-// console.log(fruta.toLowerCase()); /* MINUSCULA */
-console.log(fruta.toUpperCase());/* MAYUSCULA - () para tomar en cuenta toda la variable*/
+console.log(fruta.toLowerCase()); /* MINUSCULA */
+console.log(fruta.toUpperCase());/* MAYUSCULA
 
 console.log(fruta.length);/* cuenta los caracteres */
     
 
 
-// EJEMPLO 4
+// EJEMPLO 3
+let curso = 'javascript';/* variable */
 
-console.log(curso.slice(2,5)); /*  */
+console.log(curso.slice(2,5)); /* seleccionamos por numero y recortamos de x-hasta-y */
+console.log(curso.slice('-')); /* '-' () para tomar en cuenta toda la variable */
+console.log(curso.slice(',')); /* ',' () para separar en  palabras */
+
+
+// EJEMPLO 4
+// CONVERTIR
+
+var numero1 = '1';
+var numero2 = '3';
+
+console.log(numero1 + numero2); /* en vez de sumar, concatenear osea los une, formando 13 */
+console.log(Number(numero1) + Number(numero2)); /* asi lo convertirmos a tipo numer y lo sumamos de verdad */
---
+++ pruebas javscript/5- operadores.js
@@ -0,0 +1,36 @@
+// operadores
+
+// EJEMPLO 1 INCREMENTO/decremento
+var num1 = 5;
+    num1 = num1 + 3;/* usa como igual la anterior variable, y le suma 3 */
+    num1++;
+
+console.log(num1);
+
+// ++ incrementa en 1 el valor, osea 9
+// -- disminuye en 1, osea 7
+// 
+
+// EJEMPLO 2 DIVISION y residuo en en log
+
+var num1 = 18;
+console.log(num1 / 3);/* / para dividir */
+/* con % puedes sacar el resuiduo ej; num 1 % 3); */
+
+// EJEMPLO 3 operacion completa+booleanos?
+
+var num1 = 2;
+var num2 = 3;
+var resultado = num1 * num2;/* o puedes usar / */
+console.log(resultado)
+
+// LOS BOOLEANOS SON:
+// al usar > mayor que 
+//  al usar < menor que
+//  == igual a
+//  != distinto a (escrito como ! = )
+
+var resultado = num1 != num2;// si el valor es distinto volvera true
+
+
+console.log(resultado)
