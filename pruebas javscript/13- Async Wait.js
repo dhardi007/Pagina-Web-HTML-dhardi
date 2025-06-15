5b87174ebf9cd628997d779887fbd15485178c5b
Diego <dhardi@cincinnatus.edu.do>
2025-06-15 05:49:58 -0400

    async wait y archivos.json NOTED

---
+++ pruebas javscript/13- Async Wait.js
@@ -0,0 +1,19 @@
+// Promises y Async/Await en JavaScript
+
+// Creamos una función que devuelve una promesa
+
+function esperarUnSegundo() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("¡Pasó 1 segundo!");
+    }, 1000);
+  });
+}
+
+// Usamos async/await para esperar la promesa
+async function ejecutar() {
+  const mensaje = await esperarUnSegundo(); // Esperamos a que se resuelva
+  console.log(mensaje); // Salida después de 1 segundo: ¡Pasó 1 segundo!
+}
+
+ejecutar();
